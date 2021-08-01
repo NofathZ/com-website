db4b56ed724434d4df8255a269e48173f40e1d2d
Ramdani Kurnia <adiatma85@gmail.com>
2021-08-01 09:03:46 +0700

    Minor change in both main bodies at Company and Community

--- src/components/Community/MainBodies.js
+++ src/components/Community/MainBodies.js
@@ -9,8 +9,8 @@ function MainBodies() {
         <div className="main-body body-two d-flex align-items-center bg-emerald-green-20 text-center"><b className="mx-auto">Boards of Experts</b></div>
       </div>
       <div className="d-flex justify-content-center main-container-community" style={{columnGap: "40px"}}>
-        <div className="main-body body-two d-flex align-items-center bg-emerald-green-20 text-center"><b className="mx-auto">Boards of Advisors</b></div>
-        <div className="main-body body-two d-flex align-items-center bg-emerald-green-20 text-center"><b className="mx-auto">Boards of Advisors</b></div>
+        <div className="main-body body-two d-flex align-items-center bg-emerald-green-20 text-center"><b className="mx-auto">Boards of Directors</b></div>
+        <div className="main-body body-two d-flex align-items-center bg-emerald-green-20 text-center"><b className="mx-auto">Boards of Supervisors</b></div>
       </div>
     </div>
   )
