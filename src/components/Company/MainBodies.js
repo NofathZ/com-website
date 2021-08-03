702f73684b3e9d15ddcaf30d3c3a086a58681fa2
Ramdani Kurnia <adiatma85@gmail.com>
2021-08-03 21:34:21 +0700

    Typo fix in Main Bodies

--- src/components/Company/MainBodies.js
+++ src/components/Company/MainBodies.js
@@ -6,7 +6,7 @@ function MainBodies() {
       <div className="d-flex justify-content-center main-container" style={{columnGap: "40px", marginBottom: "40px"}}>
         <div className="main-body body-two d-flex align-items-center bg-tangerine-orange-20 text-center"><b className="mx-auto">Board of Advisors</b></div>
         <div className="main-body body-two d-flex align-items-center bg-tangerine-orange-20 text-center"><b className="mx-auto">Owner</b></div>
-        <div className="main-body body-two d-flex align-items-center bg-tangerine-orange-20 text-center"><b className="mx-auto">Boards of Experts</b></div>
+        <div className="main-body body-two d-flex align-items-center bg-tangerine-orange-20 text-center"><b className="mx-auto">Board of Experts</b></div>
       </div>
       <div className="d-flex justify-content-center main-container" style={{columnGap: "40px"}}>
         <div className="main-body body-two d-flex align-items-center bg-tangerine-orange-20 text-center"><b className="mx-auto">Board of Directors</b></div>
